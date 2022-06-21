4e6057a40b21b65abfb07b92419c33ed38f84ae5
Jef Ogues <jef.hotmail_@hotmail.com>
2022-06-21 19:54:19 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer () {
         <footer>
             <h3>Created by Jef Ogues</h3>
             <div>
-              <a href="https://github.com/jef1197/">Github</a>  
+              <a href="https://github.com/jef1197/" target="_blank" rel="noopener noreferrer">Github</a>  
               <p href="#">jef.ogu@gmail.com</p>
             </div>
         </footer>
